1c74469365bb14633fc3858cf8e06b8755b5c403
Ferdinand Emo <emoleumassi@gmail.com>
2015-03-17 22:20:15 +0100

    change data structur to JPA

---
+++ src/main/java/de/emo/cit/tuberlin/service/UDDIService.java
@@ -0,0 +1,12 @@
+package de.emo.cit.tuberlin.service;
+
+import java.util.List;
+
+import de.emo.cit.tuberlin.model.UDDI;
+
+public interface UDDIService {
+
+	public List<UDDI> listUDDI();
+	
+	public UDDI getUDDIById(int id);
+}
