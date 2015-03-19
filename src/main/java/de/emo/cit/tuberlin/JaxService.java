a77d7dbb0309703a623ec5672c6e505afd314db2
ferdinand <emoleumassi@gmail.com>
2015-03-19 17:36:25 +0100

    added transactional to serviceImpl. Change hostname of mysql

--- src/main/java/de/emo/cit/tuberlin/JaxService.java
+++ src/main/java/de/emo/cit/tuberlin/JaxService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
@@ -15,17 +16,15 @@ import de.emo.cit.tuberlin.bootstrap.ThesisConfiguration;
 import de.emo.cit.tuberlin.model.UDDI;
 import de.emo.cit.tuberlin.service.ThesisService;
 
-//@Component
 @Path("/webservice")
 @Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
 public class JaxService {
-	// implements WebApplicationInitializer {
 
 	@Autowired
 	Emo emo;
 	@Autowired
 	Track track;
-	
+
 	@SuppressWarnings("rawtypes")
 	@Autowired
 	ThesisService thesisServive;
@@ -52,17 +51,19 @@ public class JaxService {
 	@SuppressWarnings({ "unchecked", "resource" })
 	@GET
 	@Path("/all")
-	public void getAll() {
+	// @POST
+	// @PATH("/ALL/{DESCRIPTION}")
+	public Response getAll() {
 		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(
 				ThesisConfiguration.class);
 		AutowireCapableBeanFactory acbFactory = applicationContext
 				.getAutowireCapableBeanFactory();
 		acbFactory.autowireBean(this);
 
-		uddi.setDescription("This a just a test description");
+		uddi.setDescription("This a just a test description with response");
 		thesisServive.setClazz(UDDI.class);
 		thesisServive.createEntity(uddi);
-		// uddiService.listUDDI();
+		return Response.status(200).entity(uddi).build();
 	}
 
 	@GET
--- src/main/java/de/emo/cit/tuberlin/service/impl/ThesisServiceImpl.java
+++ src/main/java/de/emo/cit/tuberlin/service/impl/ThesisServiceImpl.java
@@ -8,9 +8,11 @@ import javax.persistence.PersistenceContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import de.emo.cit.tuberlin.service.ThesisService;
 
+@Transactional
 @Service("thesisService")
 public class ThesisServiceImpl<T> implements ThesisService<T> {
 
