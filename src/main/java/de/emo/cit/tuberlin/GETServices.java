eb785038c7a175f377c6e3e5a80d54dd6d8382d3
Ferdinand Emo <emoleumassi@gmail.com>
2015-03-21 22:19:29 +0100

    rename jaxservice to PostService and create getService for the http get method.

--- src/main/java/de/emo/cit/tuberlin/GETServices.java
+++ src/main/java/de/emo/cit/tuberlin/GETServices.java
@@ -2,9 +2,7 @@ package de.emo.cit.tuberlin;
 
 import java.util.Date;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -20,66 +18,27 @@ import de.emo.cit.tuberlin.bootstrap.ThesisConfiguration;
 import de.emo.cit.tuberlin.config.GenerateUUID;
 import de.emo.cit.tuberlin.model.OverviewDoc;
 import de.emo.cit.tuberlin.model.SLA;
-import de.emo.cit.tuberlin.model.ThesisRoot;
 import de.emo.cit.tuberlin.model.UDDI;
 import de.emo.cit.tuberlin.model.UDDISLA;
 import de.emo.cit.tuberlin.service.ThesisService;
 
-@Path("/webservice")
+@Path("/all")
 @Produces(MediaType.APPLICATION_JSON)
-public class JaxService {
+public class GETServices {
 
 	@Autowired
-	Emo emo;
-	@Autowired
-	Track track;
-
-	@SuppressWarnings("rawtypes")
-	@Autowired
-	ThesisService thesisServive;
-
+	private SLA sla;
 	@Autowired
 	private UDDI uddi;
 	@Autowired
 	private UDDISLA uddisla;
 	@Autowired
 	private OverviewDoc overviewDoc;
+	
+	@SuppressWarnings("rawtypes")
 	@Autowired
-	private SLA sla;
-
-	@SuppressWarnings("resource")
-	@GET
-	public Track test() {
-
-		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(
-				ThesisConfiguration.class);
-		AutowireCapableBeanFactory acbFactory = applicationContext
-				.getAutowireCapableBeanFactory();
-		acbFactory.autowireBean(this);
-
-		emo.setNom("Mon nom est Emo Leumassi Ferdinand Frederic");
-		track.setSinger("je suis le singer new singer");
-		track.setTitle("voici le titel");
-		track.setEmo(emo);
-		return track;
-	}
-
-	@SuppressWarnings({ "resource" })
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/send")
-	public Response postUDDISLA(ThesisRoot thesisRoot) {
-		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(
-				ThesisConfiguration.class, ModelConfiguration.class);
-		AutowireCapableBeanFactory acbFactory = applicationContext
-				.getAutowireCapableBeanFactory();
-		acbFactory.autowireBean(this);
-
-		setUUID(thesisRoot);
-		return Response.status(200).entity(thesisRoot).build();
-	}
-
+	ThesisService thesisServive;
+	
 	@SuppressWarnings({ "resource", "deprecation" })
 	@GET
 	@Path("/uddisla")
@@ -117,29 +76,15 @@ public class JaxService {
 		uddisla.setVersion("1.0");
 		uddisla.setUddi(uddi);
 		// sla.setUddisla(uddisla);
-		uddisla.setSla(sla);
+		// uddisla.setSla(sla);
 		setEntity(uddisla);
 		return Response.status(200).entity(uddisla).build();
 	}
-
+	
 	@SuppressWarnings("unchecked")
 	private void setEntity(Object object) {
 		thesisServive.setClazz(Object.class);
 		thesisServive.createEntity(object);
 	}
-	
-	private void setUUID(ThesisRoot thesisRoot){
-		
-		UDDISLA uddisla = thesisRoot.getUddisla();
-		uddisla.setUddislaId(GenerateUUID.newUUID());
-		
-		UDDI uddi = uddisla.getUddi();
-		uddi.setUddiId(GenerateUUID.newUUID());
-		
-		SLA sla = uddisla.getSla();
-		sla.setSlaId(GenerateUUID.newUUID());	
-		
-		OverviewDoc overviewDoc = uddi.getOverviewDoc();
-		overviewDoc.setOverviewDocId(GenerateUUID.newUUID());
-	}
+
 }
---
+++ src/main/java/de/emo/cit/tuberlin/POSTService.java
@@ -0,0 +1,79 @@
+package de.emo.cit.tuberlin;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import de.emo.cit.tuberlin.bootstrap.ModelConfiguration;
+import de.emo.cit.tuberlin.bootstrap.ThesisConfiguration;
+import de.emo.cit.tuberlin.config.GenerateUUID;
+import de.emo.cit.tuberlin.model.OverviewDoc;
+import de.emo.cit.tuberlin.model.SLA;
+import de.emo.cit.tuberlin.model.ThesisRoot;
+import de.emo.cit.tuberlin.model.UDDI;
+import de.emo.cit.tuberlin.model.UDDISLA;
+import de.emo.cit.tuberlin.service.ThesisService;
+
+@Path("/webservice")
+@Produces(MediaType.APPLICATION_JSON)
+public class POSTService {
+
+	@SuppressWarnings("rawtypes")
+	@Autowired
+	ThesisService thesisServive;
+
+	private SLA sla;
+	private UDDI uddi;
+	private UDDISLA uddisla;
+	private OverviewDoc overviewDoc;
+
+	@SuppressWarnings({ "resource" })
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/send")
+	public Response postUDDISLA(ThesisRoot thesisRoot) {
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+				ThesisConfiguration.class, ModelConfiguration.class);
+		AutowireCapableBeanFactory acbFactory = applicationContext
+				.getAutowireCapableBeanFactory();
+		acbFactory.autowireBean(this);
+
+		setUUID(thesisRoot);
+		return Response.status(200).entity(thesisRoot).build();
+	}
+
+	private void setUUID(ThesisRoot thesisRoot) {
+
+		uddisla = thesisRoot.getUddisla();
+		uddisla.setUddislaId(GenerateUUID.newUUID());
+
+		uddi = uddisla.getUddi();
+		uddi.setUddiId(GenerateUUID.newUUID());
+
+		sla = uddisla.getSla();
+		sla.setSlaId(GenerateUUID.newUUID());
+
+		overviewDoc = uddi.getOverviewDoc();
+		overviewDoc.setOverviewDocId(GenerateUUID.newUUID());
+
+		setEntity(uddisla);
+		setEntity(sla);
+		setEntity(uddi);
+		setEntity(overviewDoc);
+	}
+
+	@SuppressWarnings("unchecked")
+	private void setEntity(Object object) {
+		thesisServive.setClazz(Object.class);
+		thesisServive.createEntity(object);
+	}
+}
