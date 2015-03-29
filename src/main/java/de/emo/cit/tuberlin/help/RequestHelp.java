ae292374bfecc024734f3005d47eadaf843856bd
Ferdinand Emo <emoleumassi@gmail.com>
2015-03-29 15:29:23 +0200

    perform response message

--- src/main/java/de/emo/cit/tuberlin/help/RequestHelp.java
+++ src/main/java/de/emo/cit/tuberlin/help/RequestHelp.java
@@ -7,7 +7,7 @@ public class RequestHelp {
 
 	public static final Status BAD_REQUEST_STATUS = Response.Status.BAD_REQUEST;
 	public static String BAD_REQUEST_MESSAGE = "HTTP/1.1 "
-			+ BAD_REQUEST_STATUS.getStatusCode() + " Bad Request";
+			+ BAD_REQUEST_STATUS.getStatusCode() + " Bad Request. ";
 	
 	public static final Status OK = Response.Status.OK;
 }
